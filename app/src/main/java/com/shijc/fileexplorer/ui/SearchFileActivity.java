07b18e28573d9752cf4455ba0e535fe58a1a0546
shijiacheng <iot_shijiacheng@163.com>
2017-03-12 21:05:19 +0800

    新增搜索页

---
+++ app/src/main/java/com/shijc/fileexplorer/ui/SearchFileActivity.java
@@ -0,0 +1,15 @@
+package com.shijc.fileexplorer.ui;
+
+import android.os.Bundle;
+
+import com.shijc.fileexplorer.R;
+import com.shijc.fileexplorer.base.BaseActivity;
+
+public class SearchFileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_file);
+    }
+}
