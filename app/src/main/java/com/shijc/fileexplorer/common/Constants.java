c02ab2670a92bcb9064ebf8de1eea6c20a5705dc
shijiacheng <iot_shijiacheng@163.com>
2017-03-12 21:06:31 +0800

    新增常量类

---
+++ app/src/main/java/com/shijc/fileexplorer/common/Constants.java
@@ -0,0 +1,12 @@
+package com.shijc.fileexplorer.common;
+
+/**
+ * Class:Constants
+ * Description:常量类
+ * User: shijiacheng.
+ * Date: 2017/3/12.
+ */
+
+public class Constants {
+    public static final String KEY_EXTRA_FILE_TYPE = "key_extra_file_type";
+}
