ca5cd05d8681e1aedd9a0e1d0fd5602cd2aa86c3
shijiacheng <iot_shijiacheng@163.com>
2017-03-12 21:04:12 +0800

    全局context

--- app/src/main/java/com/shijc/fileexplorer/AppContext.java
+++ app/src/main/java/com/shijc/fileexplorer/AppContext.java
@@ -11,8 +11,16 @@ import android.app.Application;
 
 public class AppContext extends Application {
 
+    private static AppContext instance;
+
+    public static AppContext getInstance() {
+        return instance;
+    }
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
     }
 }
